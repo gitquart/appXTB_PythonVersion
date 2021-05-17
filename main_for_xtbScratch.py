5ec1129085ca841c6aaaa5e4c725acc7ac1179a6
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-17 17:00:21 -0500

    ...

--- main_for_xtbScratch.py
+++ main_for_xtbScratch.py
@@ -7,4 +7,11 @@ ws=create_connection("wss://ws.xtb.com/demo")
 ws.send(json.dumps(openFile('xtb_login.json')))
 res=json.loads(ws.recv())
 ssID=res["streamSessionId"]
-print(ssID)
+
+json_doc=openFile('cmd_no_streaming.json')
+#json_doc['streamSessionId']=ssID
+
+json_send=json.dumps(json_doc)
+ws.send(json_send)
+res=json.loads(ws.recv())
+print(res)
