838c3bc1ac697e367c2d70ee62fe0067a1ad3826
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-20 10:03:57 -0500

    ...

--- main.py
+++ main.py
@@ -6,44 +6,36 @@ from utils import *
 
 def main():
 
-    # enter your login credentials here
-   
-
-    # create & connect to RR socket
-    client = APIClient(mode='real')
-    
-    # connect to RR socket, login
+    modal='demo'
+    client = APIClient(mode=modal)
+    bstreaming=False
     loginResponse = client.execute(openFile('xtb_login.json'))
     
-
-    # check if user logged in correctly
     if(loginResponse['status'] == False):
         print('Login failed. Error code: {0}'.format(loginResponse['errorCode']))
         return
 
-    # get ssId from login response
     ssid = loginResponse['streamSessionId']
     
-
-    #client.execute(openFile('cmd_no_streaming.json'))
-    sclient = APIStreamClient(mode='real',ssID=ssid)
+    if bstreaming:
+        sclient = APIStreamClient(mode=modal,ssID=ssid)
+      
+    if bstreaming:
+        #Streaming
+        sclient.subscribe(openFile('streaming/getTickPrices.json'))
+        time.sleep(10)
+        sclient.disconnect()
+    else:    
+        #No streaming
+        client.execute(openFile('no_streaming/getCalendar.json'))
+        time.sleep(3)
+        client.disconnect()
     
     
-    sclient.subscribe(openFile('getTickPrices.json'))
-    
-
-    time.sleep(10)
-
-    #sclient.unsubscribe(openFile('cmd_unsubscribe.json'))
     
     print('The time is done.')
     
-    # gracefully close streaming socket
-    #sclient.disconnect()
-    
-    # gracefully close RR socket
-    client.disconnect()
-    
+
     
 if __name__ == "__main__":
     main()	
