02852f609091e684f7b6aa3ee1652baa3bc27aa0
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-20 10:29:44 -0500

    ...

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ from utils import *
 def main():
 
     modal='real'
-    bstreaming=False
-    
+    bstreaming=True
+    userId=''
+    pwd=''
+    if modal=='demo':
+        userId=12246157
+        pwd='judith123'
+    else:
+        userId=1832076
+        pwd='Quart2020'
+
+    loginJson=openFile('xtb_login.json')
+    loginJson['arguments']['userId']=userId
+    loginJson['arguments']['password']=pwd
     client = APIClient(mode=modal)
-    loginResponse = client.execute(openFile('xtb_login.json'))
+    loginResponse = client.execute(loginJson)
     
     if(loginResponse['status'] == False):
         print('Login failed. Error code: {0}'.format(loginResponse['errorCode']))
@@ -24,12 +35,16 @@ def main():
     if bstreaming:
         #Streaming
         sclient.subscribe(openFile('streaming/getTrades.json'))
-        time.sleep(10)
+        secs=10
+        print('Holding :',str(secs),' secs.')
+        time.sleep(secs)
         sclient.disconnect()
     else:    
         #No streaming
         client.execute(openFile('no_streaming/getTrades.json'))
-        time.sleep(3)
+        secs=3
+        print('Holding :',str(secs),' secs.')
+        time.sleep(secs)
     
     client.disconnect()
     
